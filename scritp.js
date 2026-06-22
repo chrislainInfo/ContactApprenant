f00698d6c42c47a350159b04bf8b03cf2e7a510d
chislain <adelininfo08@gmail.com>
2026-06-22 17:39:03 +0100

    ajout de la colonne telephone et de la logique des doublants

--- scritp.js
+++ scritp.js
@@ -1,4 +1,4 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzEf9VvqLVV7mAwZzKlziudi0xKYNR4opKZcahN-uxBDMn2ju2p3EPginZbtBQxD6MoeQ/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzbzE2xrFeWZLwmw-mB4ShTrQhrvGEc5jDSibEPXuNdVeclToex0xV7wAoyNFGbHX4jpA/exec";
 
 document.getElementById('contactForm').addEventListener('submit', function(e) {
     e.preventDefault();
@@ -12,29 +12,43 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
     const formData = {
         nom: document.getElementById('nom').value,
         prenom: document.getElementById('prenom').value,
-        email: document.getElementById('email').value
+        email: document.getElementById('email').value,
+        tel: "" + document.getElementById('tel').value
     };
 
-   
     fetch(GOOGLE_SCRIPT_URL, {
         method: 'POST',
-        mode: 'no-cors', 
-        cache: 'no-cache',
-        headers: {
-            'Content-Type': 'application/json'
-        },
         body: JSON.stringify(formData)
     })
-    .then(() => {
-        responseMessage.className = "message success";
-        responseMessage.innerText = "Merci ! Vos informations ont bien été enregistrées.";
-        
-        document.getElementById('contactForm').reset();
+    .then(res => res.json())
+    .then(response => {
+        if (response.status === "success") {
+
+            responseMessage.className = "message success";
+            responseMessage.innerText = "Merci ! Vos informations ont bien été enregistrées.";
+            document.getElementById('contactForm').reset();
+
+            const time = setTimeout(() => {
+                responseMessage.className = "message";
+                responseMessage.innerText = ""; 
+                clearTimeout(time) 
+            }, 4000);
+
+        } else if (response.status === "duplicate") {
+
+            responseMessage.className = "message error";
+            responseMessage.innerText = "Désolé, cet e-mail est déjà inscrit sur notre liste.";
+            
+        } else {
+            responseMessage.className = "message error";
+            responseMessage.innerText = response.message + "veillez reessayer"
+
+        }
     })
     .catch(error => {
-
         responseMessage.className = "message error";
-        responseMessage.innerText = "Une erreur est survenue. Veuillez réessayer.";
+        responseMessage.innerText = "Une erreur est survenue, veillez reessayer"
+        
         console.error('Erreur:', error);
     })
     .finally(() => {
